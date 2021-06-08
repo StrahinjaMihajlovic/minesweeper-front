0b8d6fcbe9582bd2d82560f1898b6c4b972a4dad
Strahinja <strahinja.mihajlovic@quantox.com>
2021-06-08 14:34:20 +0200

    Added variable menu items that depends if user is logged in or not.

--- src/components/Menu.js
+++ src/components/Menu.js
@@ -1,14 +1,20 @@
 import React from 'react';
 import {NavLink} from "react-router-dom";
+import AppConfig from '../config/AppConfig.js';
+import Authorization from './auth/Authorization.js'
+class Menu extends Authorization {
+    componentDidMount() {
+        this.isLoggedIn();
+    }
 
-class Menu extends React.Component {
+
     render() {
         return (
             <nav className='flex h-8 bg-gradient-to-r from-purple-300 via-red-300 to-blue-300'>
                 <NavLink to='/' className='flex-1 border-r-2 '>Home</NavLink>
-                <NavLink to='/login' className='flex-1 border-r-2'>Login</NavLink>
-                <NavLink to='/register' className='flex-1 border-r-2'>register</NavLink>
-                <NavLink to='/login' className='flex-1 border-r-2'>logout</NavLink>
+                {!AppConfig.isLoggedIn ? <NavLink to='/login' className='flex-1 border-r-2'>Login</NavLink> : ''}
+                {!AppConfig.isLoggedIn ? <NavLink to='/register' className='flex-1 border-r-2'>register</NavLink> : '' }
+                {AppConfig.isLoggedIn ? <NavLink to='/logout' className='flex-1 border-r-2'>logout</NavLink> : '' }
             </nav>
         );
     }

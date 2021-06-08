8abedc5356a9ab58c1b12f0d51c02e6623d698e7
Strahinja <strahinja.mihajlovic@quantox.com>
2021-06-08 15:20:56 +0200

    refactored code after registration form and routes

--- src/components/RenderPageFromUrl.js
+++ src/components/RenderPageFromUrl.js
@@ -1,15 +1,35 @@
 import { Switch, Route } from "react-router-dom/cjs/react-router-dom.min";
 import Login from "./auth/Login";
-import DefaultEndPoint from "./DefaultEndPoint";
+import Registration from './auth/Registration.js';
 import React from 'react';
+import AppConfig from "../config/AppConfig";
+import App from "../App";
 
 class RenderPageFromUrl extends React.Component {
+
+
+    renderProtectedPaths() {
+        return (
+            <React.Fragment>
+                <Route path='/'>Hello</Route>
+                <Route path='/logout' >{}</Route>
+            </React.Fragment>
+        );
+    }
+
+    renderUnprotectedPaths() {
+        return (
+            <React.Fragment>
+                <Route path='/register'><Registration/></Route>
+                <Route path='/login'><Login /></Route>
+            </React.Fragment>
+        );
+    }
+
     render() {
         return (
             <Switch>
-                <Route path='/register'>Hi</Route>
-                <Route path='/login'><Login /></Route>
-                <Route path='/'>Hello</Route>
+                {AppConfig.isLoggedIn ? this.renderProtectedPaths() : this.renderUnprotectedPaths()}
             </Switch>
         );
     } 

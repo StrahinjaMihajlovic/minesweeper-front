43fac1b3996bbdc4236b23899df9a22f447bcd10
Strahinja <strahinja.mihajlovic@quantox.com>
2021-06-08 16:51:38 +0200

    Implemented logout procedure

--- src/components/RenderPageFromUrl.js
+++ src/components/RenderPageFromUrl.js
@@ -1,18 +1,27 @@
-import { Switch, Route } from "react-router-dom/cjs/react-router-dom.min";
+import { Switch, Route, withRouter} from "react-router-dom/cjs/react-router-dom.min";
 import Login from "./auth/Login";
 import Registration from './auth/Registration.js';
 import React from 'react';
 import AppConfig from "../config/AppConfig";
-import App from "../App";
+import axios from "axios";
 
 class RenderPageFromUrl extends React.Component {
 
+    redirectLoogout() {
+        if(this.props.history.location.pathname.includes('/logout')){
+            axios.post(AppConfig.backUrl + '/logout'); 
+            AppConfig.isLoggedIn = false;
+            document.cookie = 'token= ; expires=Thu, 01 Jan 1970 00:00:00 GMT';
+            this.props.history.push('/login');
+        }
+    }
 
     renderProtectedPaths() {
+       this.redirectLoogout();
         return (
             <React.Fragment>
                 <Route path='/'>Hello</Route>
-                <Route path='/logout' >{}</Route>
+                <Route path='/logout' />
             </React.Fragment>
         );
     }
@@ -21,7 +30,7 @@ class RenderPageFromUrl extends React.Component {
         return (
             <React.Fragment>
                 <Route path='/register'><Registration/></Route>
-                <Route path='/login'><Login /></Route>
+                <Route path='/'><Login /></Route>
             </React.Fragment>
         );
     }
@@ -35,4 +44,4 @@ class RenderPageFromUrl extends React.Component {
     } 
 }
 
-export default RenderPageFromUrl;
+export default withRouter(RenderPageFromUrl);

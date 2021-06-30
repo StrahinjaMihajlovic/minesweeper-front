77180c6fe23aeeb33d6a06d54dd310e404d20cd2
Strahinja Mihajlovic <strahinja.mihajlovic@quantox.com>
2021-06-30 15:37:25 +0200

    Added game and store pages to the menu

--- src/components/Menu.js
+++ src/components/Menu.js
@@ -11,9 +11,10 @@ class Menu extends Authorization {
     render() {
         return (
             <nav className='flex h-8 bg-gradient-to-r from-purple-300 via-red-300 to-blue-300'>
-                <NavLink to='/' className='flex-1 border-r-2 '>Home</NavLink>
                 {!AppConfig.getState().isLoggedIn ? <NavLink to='/login' className='flex-1 border-r-2'>Login</NavLink> : ''}
                 {!AppConfig.getState().isLoggedIn ? <NavLink to='/register' className='flex-1 border-r-2'>register</NavLink> : '' }
+                {AppConfig.getState().isLoggedIn ? <NavLink to='/game' className='flex-1 border-r-2'>game</NavLink> : '' }
+                {AppConfig.getState().isLoggedIn ? <NavLink to='/store' className='flex-1 border-r-2'>store</NavLink> : '' }
                 {AppConfig.getState().isLoggedIn ? <NavLink to='/logout' className='flex-1 border-r-2'>logout</NavLink> : '' }
             </nav>
         );

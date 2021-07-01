6ce59a103623b9f8365757f562dd4fb33109cdd4
Strahinja Mihajlovic <strahinja.mihajlovic@quantox.com>
2021-07-01 11:40:54 +0200

    Implemented builder in the game

--- src/components/game/Row.js
+++ src/components/game/Row.js
@@ -1,6 +1,6 @@
 import React, { useState } from 'react';
 import Field from './Field';
-
+import FieldBuilder from './FieldBuilder';
 const Row = (props) => {
     return (<div className='flex'>{renderFields(props.rowSize, props.data, props.fieldclick)}</div>);
 }
@@ -10,7 +10,8 @@ function renderFields(rowSize, data, action) {
     let fields = [];
     for(let i = 1; i <= +rowSize; i++) {
         let fieldNumber = `${i}`;
-        fields.push(<Field key={fieldNumber} value={fieldNumber} data={data[i - 1]} fieldclick={action}/>);
+        let builder = new FieldBuilder();
+        fields.push(<Field key={fieldNumber} data={builder.buildForGame(data[i-1].id, fieldNumber, data[i-1].contains, action)}/>);
     }
 
     return fields;

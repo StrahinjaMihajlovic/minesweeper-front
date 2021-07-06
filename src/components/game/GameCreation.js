497d5bb76d458b0efbe0b4848cc6b5152fa403d4
Strahinja Mihajlovic <strahinja.mihajlovic@quantox.com>
2021-07-06 10:04:40 +0200

    Fixed a bug where page would break when incrementing y axis more than 2 at a time. Implemented proper array manipulation ti mirror changes in the table

--- src/components/game/GameCreation.js
+++ src/components/game/GameCreation.js
@@ -1,42 +1,47 @@
 import axios from 'axios';
-import React, { useState } from 'react';
+import React, { useEffect, useState } from 'react';
 import AppConfig from '../../config/AppConfig';
 import Field from './Field';
 import FieldBuilder from './FieldBuilder';
+import { useLayoutEffect } from 'react';
 
 const GameCreation = (props) => {
     const [isCreated, setIsCreated] = useState(false);
-    const [xNumber, setXNumber] = useState(5);
-    const [yNumber, setYNumber] = useState(5);
+    const [xNumber, setXNumber] = useState({value: 5});
+    const [yNumber, setYNumber] = useState({value: 5});
     //fils the bombs object with array of bombs at specific positions. The array imitates displayed table
     const [bombs, setBombs] = useState({bombs: Array(5).fill().map(() => { return Array(5).fill(false) } ) });
+
+    useLayoutEffect(() => {setSize(xNumber.value, yNumber.value, bombs.bombs, setBombs)}, [xNumber, yNumber]);
+
     return (
         <div>
-            <button className='inline-block' onClick={() => {sendRequestForCreation(xNumber, yNumber, bombs.bombs.flat(), setIsCreated)}}><h1 className='pt-6 '>Create a new game</h1></button>
+            <button className='mt-6 border-4 border-purple-400 inline-block hover:bg-red-300 align-middle' onClick={() => {sendRequestForCreation(xNumber.value, yNumber.value, bombs.bombs.flat(), setIsCreated)}}>
+                 <p className=' p-4'>Create a new game</p>
+            </button>
+
             {isCreated ?<p className='text-green-700'>The game has been successfully created!</p> : ''}
+
             <div>
                 <p className='my-12'>Enter size of the table axises</p>
                 <div id='axis-control' className='flex justify-center my-10'>
                     <label htmlFor='x-axis'>X axis</label>
-                    <input value={xNumber} className='border-2  mx-6' type='number' name='x-axis' onChange={ event => {
-                            setXNumber(event.target.value);
-                            
+                    <input value={xNumber.value} className='border-2  mx-6' type='number' name='x-axis' onChange={ event => {
+                            setXNumber({value: parseInt(event.target.value)});
                         }}/>
                     <label htmlFor='y-axis'>Y axis</label>
-                    <input value={yNumber} className='border-2 mx-6' type='number' name='y-axis' onChange={event => {
-                            let newFields = bombs.bombs;
-                            setYNumber(event.target.value);
-                            if(newFields.length < parseInt(event.target.value)) {
-                                
-                                newFields[event.target.value - 1] = Array(xNumber).fill(false);
-                                setBombs({bombs: newFields});
+                    <input value={yNumber.value} className='border-2 mx-6' type='number' name='y-axis' onChange={async event => {
+                            let newYValue = parseInt(event.target.value);
+                            // calls setSize before changing yNumber state that re-renders the component. Size needs to be set before re-render
+                            if(bombs.bombs.length < newYValue) { 
+                                await setSize(xNumber.value, newYValue, bombs.bombs, setBombs);
                             }
-                            
+                            setYNumber({value: newYValue});
                         }}/>
                 </div>
             </div>
             <div>
-            {generateFields(xNumber, yNumber, bombs, setBombs)}
+            {generateFields(xNumber.value, yNumber.value, bombs, setBombs)}
             </div>
         </div>
     );
@@ -44,16 +49,16 @@ const GameCreation = (props) => {
 
 function sendRequestForCreation(x, y, bombs, setIsCreated) {
     axios.post(AppConfig.getState().backUrl + '/game/create', {x: x, y: y, bombs: bombs}).then(response => {
-        setIsCreated(true);
+        console.log(response.data)
+        //setIsCreated(true);
     }).catch(err => {
-        'error'; //TODO implement error handling
+        console.log(err.response.data); //TODO implement error handling
     });
 }
 
 // generates fields for a game in progress given size and bombs parameters
 function generateFields(sizeX, sizeY, bombs, setBombs) {
     let rows = [];
-
     for(let i = 1; i <= sizeY; i++) {
         let fields = []
        for(let j = 1; j <= sizeX; j++) {
@@ -73,8 +78,27 @@ function plantTheMine(bombs, setBombs, fieldX, fieldY) {
     setBombs({bombs: newBombs});
 }
 
-async function setSize() {
-
+// changes size of the bombs fields to mirror created table
+async function setSize(x, y, bombs, setBombs) {
+    bombs.forEach((elements, key, bombs) => {
+        let length = elements.length;
+        if(elements.length < x) { // manipulates x axis size of the table
+            for(let i = 0; i < (x - length); i++) {
+                elements.push(false);
+            }
+        } else {
+            bombs[key] = elements.slice(0, x);
+        }
+    });
+    let length = bombs.length;
+    if(length < y) { //manipulates y axis size of the table
+        for(let i = 0; i < (y - length); i++) {
+            bombs.push(Array(x).fill(false));
+        }
+    } else {
+        bombs = bombs.slice(0, y);
+    }
+    setBombs({bombs: bombs});
 }
 
 export default GameCreation;
